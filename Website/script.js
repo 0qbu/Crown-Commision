5d7db1837339722ad424d08954a0bb69bb2cfc4c
nikolozshotadze <150133474+nikolozshotadze@users.noreply.github.com>
2024-01-22 23:55:40 -0800

    Update script.js

---
+++ Website/script.js
@@ -0,0 +1,17 @@
+var style = "font-size: 24px;" +
+  "background: #67b26f; /* fallback for old browsers */" + 
+  "background: -webkit-linear-gradient(to right, #67b26f, #4ca2cd); /* Chrome 10-25, Safari 5.1-6 */" +
+  "background: linear-gradient(to right, #67b26f, #4ca2cd); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */" +
+  "color: white;" +
+  "text-align: center;" +
+  "padding: 10px 15px;" +
+  "width: 100%;" +
+  "border-radius: 20px;";
+
+var text = "%cMade by pozwander🙏. Add me on discord 😉 User @pozwander";
+
+console.log(text, style);
+
+/* eslint no-console: ["error", { allow: ["log", "info", "warn"] }] */
+
+/* Prints the app name and version, helpful for debugging */
